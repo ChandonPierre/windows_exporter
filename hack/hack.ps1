00605fbd5d6d5f9ca76eabd9703ed6cff49520ab
ChandonPierre <cpierre@coreweave.com>
2022-09-01 18:42:36 -0400

    chore: add hack build script

---
+++ hack/hack.ps1
@@ -0,0 +1,11 @@
+﻿@('mingw','go') | %{iex -Command $('choco '+'install'+' '+$_+' -y --no-progress')}
+
+go install github.com/prometheus/promu@v0.13.0
+
+git clone https://github.com/coreweave/windows_exporter
+
+git checkout pr/817
+
+promu build
+
+.\installer\build.ps1 -PathToExecutable .\windows_exporter.exe -Version 0.19.3 -Arch amd64
